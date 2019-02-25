ceff2a1239e8487c9a2befacb1da9e05e090298c
wdias <wd61@uol.com.br>
2019-02-25 18:52:11 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -9,4 +9,13 @@ public class Utils {
 	public static Stage currentSatage(ActionEvent event) {
 		return  (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
+	
+	public static Integer tryParseToInt (String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
 }
